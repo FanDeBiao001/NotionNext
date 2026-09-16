f3ac9887574304894278a5d414ba392ddb08ff4a
CodeFree001 <zhang9494kuo@gmail.com>
2026-09-16 23:02:45 +0800

    修复上一页/下一页按钮问题

--- themes/matery/components/PaginationSimple.js
+++ themes/matery/components/PaginationSimple.js
@@ -4,56 +4,61 @@ import { useRouter } from 'next/router'
 /**
  * 简易翻页插件
  * @param page 当前页码
- * @param showNext 是否有下一页
+ * @param totalPage 总页数
  * @returns {JSX.Element}
  * @constructor
  */
 const PaginationSimple = ({ page, totalPage }) => {
   const router = useRouter()
   const currentPage = +page
+  const showPrevious = currentPage > 1
   const showNext = currentPage < totalPage
   const pagePrefix = router.asPath
     .split(/[?#]/)[0]
     .replace(/\/page\/[1-9]\d*/, '')
     .replace(/\/$/, '')
   return (
-    <div className="my-10 mx-6 flex justify-between font-medium text-black dark:text-gray-100 space-x-2">
-      <SmartLink
-        href={{
-          pathname:
-            currentPage - 1 === 1
-              ? `${pagePrefix}/`
-              : `${pagePrefix}/page/${currentPage - 1}`,
-          query: router.query.s ? { s: router.query.s } : {},
-          hash: 'posts-wrapper'
-        }}
-        passHref
-        legacyBehavior>
-        <button
-          rel="prev"
-          className={`${
-            currentPage === 1 ? 'opacity-20  bg-gray-200  text-gray-500 pointer-events-none ' : 'block text-white bg-indigo-700'
-          } duration-200 px-3.5 py-2 hover:border-black rounded-full`} >
-          <i className='fas fa-angle-left text-2xl'/>
-        </button>
-      </SmartLink>
-      <SmartLink
-        href={{
-          pathname: `${pagePrefix}/page/${currentPage + 1}`,
-          query: router.query.s ? { s: router.query.s } : {},
-          hash: 'posts-wrapper'
-        }}
-        passHref
-        legacyBehavior>
-        <button
-          rel="next"
-          className={`${
-            +showNext ? 'text-white bg-indigo-700 ' : ' opacity-20 bg-gray-200 text-gray-500 pointer-events-none '
-          } duration-200 px-4 py-2 hover:border-black rounded-full`}
-        >
-          <i className='fas fa-angle-right text-2xl'/>
-        </button>
-      </SmartLink>
+    <div className='my-10 mx-6 grid grid-cols-2 items-center font-medium text-black dark:text-gray-100'>
+      {showPrevious && (
+        <div className='justify-self-start'>
+          <SmartLink
+            href={{
+              pathname:
+                currentPage === 2
+                  ? `${pagePrefix}/`
+                  : `${pagePrefix}/page/${currentPage - 1}`,
+              query: router.query.s ? { s: router.query.s } : {},
+              hash: 'posts-wrapper'
+            }}
+            passHref
+            legacyBehavior>
+            <button
+              rel='prev'
+              className='block px-3.5 py-2 text-white bg-indigo-700 hover:border-black rounded-full duration-200'>
+              <i className='fas fa-angle-left text-2xl' />
+            </button>
+          </SmartLink>
+        </div>
+      )}
+
+      {showNext && (
+        <div className='col-start-2 justify-self-end'>
+          <SmartLink
+            href={{
+              pathname: `${pagePrefix}/page/${currentPage + 1}`,
+              query: router.query.s ? { s: router.query.s } : {},
+              hash: 'posts-wrapper'
+            }}
+            passHref
+            legacyBehavior>
+            <button
+              rel='next'
+              className='block px-4 py-2 text-white bg-indigo-700 hover:border-black rounded-full duration-200'>
+              <i className='fas fa-angle-right text-2xl' />
+            </button>
+          </SmartLink>
+        </div>
+      )}
     </div>
   )
 }
